fb8d96c90d4cac747d67db90b93d2ed8eb3a22b5
mwojewodzki <mwojewodzki@pzu.pl>
2021-04-15 22:25:53 +0200

    Dodanie MwoTestTest

---
+++ src/test/java/org/mwo/MwoTestTest.java
@@ -0,0 +1,19 @@
+package org.mwo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MwoTestTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void sysout() {
+        assertTrue(true);
+    }
+}
